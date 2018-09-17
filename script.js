a1020cfabd508066a279e1d474741fcd1ffe515e
eluo0 <crystalluo1993@gmail.com>
2018-09-16 18:16:47 -0700

    update

---
+++ script.js
@@ -0,0 +1,36 @@
+
+    var text = document.querySelector(".scentery");
+    var overlay = document.querySelector(".overlay");
+      var overlay2 = document.querySelector(".overlay2");
+    var text2 = document.querySelector(".cometskirt");
+    var overlay3 = document.querySelector(".overlay3");
+  var text3 = document.querySelector(".mooder");
+
+overlay.addEventListener("mouseover", function(){
+  text.style.display="block";
+  text.style.color = "black";
+  text.style.opacity = "1.0";
+
+})
+overlay.addEventListener("mouseout", function(){
+  text.style.display="none";
+})
+
+overlay2.addEventListener("mouseover", function(){
+  text2.style.display="block";
+  text2.style.color = "black";
+  text2.style.opacity = "1.0";
+
+})
+overlay2.addEventListener("mouseout", function(){
+  text2.style.display="none";
+})
+overlay3.addEventListener("mouseover", function(){
+  text3.style.display="block";
+  text3.style.color = "black";
+  text3.style.opacity = "1.0";
+
+})
+overlay3.addEventListener("mouseout", function(){
+  text3.style.display="none";
+})
